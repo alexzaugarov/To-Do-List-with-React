6d8d33fc585b4225bc2f1fe11d98c5488957ccdd
Alexander Zaugarov <alex52711@gmail.com>
2016-12-26 18:46:20 +0700

    Added Index.cshtml view for main page. Installed requirement package for React, Webpack and Babel. Implemented "Hello world" app.

--- ToDoList/src/ToDoList.Web/Startup.cs
+++ ToDoList/src/ToDoList.Web/Startup.cs
@@ -37,7 +37,13 @@ namespace ToDoList.Web
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseMvc();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
---
+++ ToDoList/src/ToDoList.Web/webpack.config.js
@@ -0,0 +1,19 @@
+﻿var path = require("path");
+
+module.exports = {
+    entry: path.join(__dirname, "App", "main.jsx"),
+    output: {
+        path: __dirname + "/wwwroot/builds/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            include: path.join(__dirname, "App"),
+            loader: "babel",
+            query: {
+                presets: ["es2015", "react"]
+            }
+        }]
+    }
+};
