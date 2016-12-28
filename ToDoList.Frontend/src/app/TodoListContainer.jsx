91eecc59afe17a9d99c061afd9ed75080d018ae0
Alexander Zaugarov <alex52711@gmail.com>
2016-12-28 19:30:59 +0700

    Working on Redux implementation.

---
+++ ToDoList.Frontend/src/app/TodoListContainer.jsx
@@ -0,0 +1,39 @@
+﻿import React from 'react';
+import {render} from 'react-dom';
+import TaskList from "./components/list-todo.jsx";
+import Header from "./components/create-todo-panel.jsx";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            taskItems: [
+                {description: "asd"},
+                {description: "qwe"},
+                {description: "zxc"}
+            ]
+        };
+
+        this.onTaskCreated = this.onTaskCreated.bind(this);
+        console.log(typeof this.onTaskCreated);
+    }
+
+    onTaskCreated(description) {
+        this.setState(prevState => ({
+            taskItems: prevState.taskItems.concat([{description: description}])
+        }));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="col-md-4 center-block">
+                    <Header onTaskCreated={this.onTaskCreated}/>
+                    <TaskList taskItems={this.state.taskItems}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, document.getElementById('root'));
--- ToDoList.Frontend/src/app/main.jsx
+++ ToDoList.Frontend/src/app/main.jsx
@@ -1,39 +1,9 @@
-﻿import React from 'react';
-import {render} from 'react-dom';
-import TaskList from "./components/list-todo.jsx";
-import Header from "./components/create-todo-panel.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            taskItems: [
-                {description: "asd"},
-                {description: "qwe"},
-                {description: "zxc"}
-            ]
-        };
-
-        this.onTaskCreated = this.onTaskCreated.bind(this);
-        console.log(typeof this.onTaskCreated);
-    }
-
-    onTaskCreated(description) {
-        this.setState(prevState => ({
-            taskItems: prevState.taskItems.concat([{description: description}])
-        }));
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="col-md-4 center-block">
-                    <Header onTaskCreated={this.onTaskCreated}/>
-                    <TaskList taskItems={this.state.taskItems}/>
-                </div>
-            </div>
-        );
-    }
-}
-
-render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>{router}</Provider>,
+    document.getElementById('root')
+);
