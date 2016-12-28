2a42b0450d8196b3d128e1e28cdd28d7c5b85b5b
Alexander Zaugarov <alex52711@gmail.com>
2016-12-28 13:26:00 +0700

    Removed unnecessary files from WebApi.

--- ToDoList/src/ToDoList.Web/App/TodoList/Components/TodoItem.jsx
+++
@@ -1,7 +0,0 @@
-﻿import React from 'react';
-
-export default class TodoItem extends React.Component {
-    render() {
-        return <div className="task-item">{this.props.description}</div>;
-    }
-}
